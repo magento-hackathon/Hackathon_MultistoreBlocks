10582c5315af3b0a8be2717cfaa34e537d37f3f3
Jeroen Boersma <jeroen@srcode.nl>
2015-05-29 13:35:43 +0200

    Added rewrite on cms adminhtml grid

---
+++ app/code/community/Hackathon/MultistoreBlocks/Block/Adminhtml/Cms/Block/Grid.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ *
+ */
+
+/**
+ * @package Hackathon_MultistoreBlocks
+ * @category Hackathon
+ */
+class Hackathon_MultistoreBlocks_Block_Adminhtml_Cms_Block_Grid extends Mage_Adminhtml_Block_Cms_Block_Grid
+{
+
+}
