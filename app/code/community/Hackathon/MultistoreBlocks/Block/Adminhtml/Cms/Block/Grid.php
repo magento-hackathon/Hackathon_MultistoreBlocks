503b1c85cf34eda663639993e30b337c3827fe06
Jeroen Boersma <jeroen@srcode.nl>
2015-05-29 14:39:13 +0200

    Added store ids over all blocks

--- app/code/community/Hackathon/MultistoreBlocks/Block/Adminhtml/Cms/Block/Grid.php
+++ app/code/community/Hackathon/MultistoreBlocks/Block/Adminhtml/Cms/Block/Grid.php
@@ -27,4 +27,37 @@ class Hackathon_MultistoreBlocks_Block_Adminhtml_Cms_Block_Grid extends Mage_Adm
         return Mage_Adminhtml_Block_Widget_Grid::_prepareCollection();
     }
 
+    /**
+     * Add all store ids
+     *
+     * @return $this
+     */
+    protected function _afterLoadCollection()
+    {
+        $collection = $this->getCollection();
+        /** @var $collection Mage_Cms_Model_Resource_Block_Collection */
+
+        $connection = $collection->getConnection();
+
+        // Add store ids
+        foreach ($collection as $cmsBlock) {
+            /** @var $cmsBlock Hackathon_MultistoreBlocks_Model_Block */
+
+            $select = $connection->select();
+
+            $select
+                ->from(array('cb' => $collection->getTable('cms/block')), array())
+                ->join(array('cbs' => $collection->getTable('cms/block_store')), 'cb.block_id = cbs.block_id', array('store_id'))
+                ->where('cb.identifier = ?', $cmsBlock->getData('identifier'))
+                ->distinct();
+
+            $storeIds = $connection->fetchCol($select);
+
+            $cmsBlock->setStoreId($storeIds)
+                ->setStores($storeIds);
+        }
+
+        return $this;
+    }
+
 }
