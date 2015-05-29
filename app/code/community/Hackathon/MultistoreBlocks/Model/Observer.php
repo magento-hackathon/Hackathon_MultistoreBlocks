489fdea15b241e1f10d27cebbb1eca44d251edbe
Peter Jaap Blaakmeer <peterjaap@elgentos.nl>
2015-05-29 14:09:19 +0200

    Added return while devving

--- app/code/community/Hackathon/MultistoreBlocks/Model/Observer.php
+++ app/code/community/Hackathon/MultistoreBlocks/Model/Observer.php
@@ -17,13 +17,28 @@ class Hackathon_MultistoreBlocks_Model_Observer
         Mage::register('before_save_cms_block_prevent_loop', true);
 
         $block = $observer->getEvent()->getDataObject();
-        //Zend_Debug::dump($block);exit;
+        return;
+
         foreach($block->getContent() as $key=>$content)
         {
-            $status = $block->getStatus()[$key]; // upgrade to 5.5 instead of changing this, lazy bastard
+            $isActive = $block->getIsActive()[$key]; // upgrade to 5.5 instead of changing this, lazy bastard
             $storeIds = $block->getStoreId()[$key];
+            $existingId = $block->getExistingId[$key];
 
+            foreach($storeIds as $storeId)
+            {
+                $_block = Mage::getModel('cms/block')->setStoreId($storeId)->load($block->getIdentifier());
+                if($existingId)
+                {
 
+                }
+                $_block->setTitle($block->getTitle());
+                $_block->setIdentifier($block->getIdentifier());
+                $_block->setIsActive($isActive);
+                $_block->setStores($storeIds);
+                $_block->setContent($content);
+                $_block->save();
+            }
         }
     }
 
