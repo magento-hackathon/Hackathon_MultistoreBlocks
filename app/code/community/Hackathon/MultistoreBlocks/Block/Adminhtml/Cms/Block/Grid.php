a50e3927921490755cb098ef76990d99240621db
Jeroen Boersma <jeroen@srcode.nl>
2015-05-29 14:02:58 +0200

    Grouped by identifier
    + grouped by identifier

--- app/code/community/Hackathon/MultistoreBlocks/Block/Adminhtml/Cms/Block/Grid.php
+++ app/code/community/Hackathon/MultistoreBlocks/Block/Adminhtml/Cms/Block/Grid.php
@@ -1,6 +1,6 @@
 <?php
 /**
- *
+ * Rework to group by identifier
  */
 
 /**
@@ -9,5 +9,22 @@
  */
 class Hackathon_MultistoreBlocks_Block_Adminhtml_Cms_Block_Grid extends Mage_Adminhtml_Block_Cms_Block_Grid
 {
+
+    /**
+     * Prepare collection and group by
+     * @return $this
+     */
+    protected function _prepareCollection()
+    {
+        $collection = Mage::getModel('cms/block')->getCollection();
+        /** @var $collection Mage_Cms_Model_Resource_Block_Collection */
+
+        $this->setCollection($collection);
+        // Group by identifier
+        $collection->getSelect()->group('identifier');
+
+        // Skip parent to force grouping
+        return Mage_Adminhtml_Block_Widget_Grid::_prepareCollection();
+    }
 
 }
