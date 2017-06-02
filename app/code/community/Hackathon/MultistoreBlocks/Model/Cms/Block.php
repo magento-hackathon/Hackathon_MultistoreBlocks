e80849eeedb09d9f8e03211d492a5ba2180f8bcc
Sander Jongsma <sanderjongsma90@gmail.com>
2017-06-02 16:23:41 +0200

    Add storenames to options array (#22)

--- app/code/community/Hackathon/MultistoreBlocks/Model/Cms/Block.php
+++ app/code/community/Hackathon/MultistoreBlocks/Model/Cms/Block.php
@@ -16,4 +16,29 @@ class Hackathon_MultistoreBlocks_Model_Cms_Block extends Mage_Cms_Model_Block
 
     protected $_eventPrefix = 'cms_block';
 
+    /**
+     * @return array|string
+     */
+    public function getStoreNames()
+    {
+        $resource = Mage::getModel('core/resource');
+        $db = $resource->getConnection('core_write');
+
+        if  (!$this->getId()) {
+            return [];
+        }
+
+        $storeNames = $db->fetchCol($db->select()->from($resource->getTableName('core_store'), 'name')->joinInner($resource->getTableName('cms_block_store'), $resource->getTableName('cms_block_store') . '.store_id = ' . $resource->getTableName('core_store') . '.store_id')->where('block_id = ?', $this->getId()));
+
+        $storeNames = array_map(function ($input) {
+            if ($input == Mage::helper('cms')->__('Admin')) {
+                return Mage::helper('cms')->__('All');
+            } else {
+                return $input;
+            }
+        }, $storeNames);
+
+        return implode(', ', $storeNames);
+    }
+
 }
